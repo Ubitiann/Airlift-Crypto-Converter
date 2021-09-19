80ad581511982e9ddf91dabfd48f5005cdbe0eb6
Abdul Basit <Abdulbasitdj@gmail.com>
2021-09-19 03:11:48 -0700

    -project configuration

---
+++ app/src/main/java/com/airlift/airliftcryptoconverter/view/CryptoListActivity.kt
@@ -0,0 +1,12 @@
+package com.airlift.airliftcryptoconverter.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.airlift.airliftcryptoconverter.R
+
+class CryptoListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crypto_list)
+    }
+}
